67b7a561a2eb0319cd02777a5ea8971aed3ccb8f
Wenlei Cai <35846257+wcai49@users.noreply.github.com>
2024-05-22 23:29:54 +0800

    use hashrouter.

--- cinemachine-docs-chinese/src/index.js
+++ cinemachine-docs-chinese/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
